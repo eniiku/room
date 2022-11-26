54bb9f762a5a45a9e3bc05934eff8277a410bc5b
eniiku <enikuauth@gmail.com>
2022-11-26 18:54:07 +0100

    style(About): add basic css declarations for page-markup

--- src/views/About/About.tsx
+++ src/views/About/About.tsx
@@ -6,10 +6,12 @@ const About = () => {
     <section aria-label='about section for the rooms homepage'>
       <img src={aboutImgDark} alt='product from the rooms catalogue' />
 
-      <div>
-        <h1>About our furniture</h1>
+      <div className='container-gutters py-12'>
+        <h1 className='text-lg uppercase font-bold tracking-[.4rem]'>
+          About our furniture
+        </h1>
 
-        <p>
+        <p className='text-[var(--dark-gray)] text-lg mt-4 text-left'>
           Our multifunctional collection blends design and function to suit your
           individual taste. Make each room unique, or pick a cohesive theme that
           best express your interests and what inspires you. Find the furniture
