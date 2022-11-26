f12db88c3d8ba1e5f30077ae6e6481d632ab2c17
eniiku <enikuauth@gmail.com>
2022-11-26 18:20:31 +0100

    feat(Hero): create logic for cycling through slides

--- src/views/Hero/Hero.tsx
+++ src/views/Hero/Hero.tsx
@@ -1,26 +1,55 @@
-import { Navbar } from './Navbar';
+import { useRef, useState } from 'react';
 
+import { Navbar } from './Navbar';
+import { heroSlides } from '../../utils/data/heroSlides';
 import arrowIcon from '../../assets/icon-arrow.svg';
+import leftChevronIcon from '../../assets/icon-angle-left.svg';
+import rightChevronIcon from '../../assets/icon-angle-right.svg';
 
 export const Hero = () => {
+  const [slides, setSlides] = useState(heroSlides[0]);
+  const [index, setIndex] = useState(0);
+  console.log(index);
+
+  const previousSlide = () => {
+    if (index >= 1 && index < 3) {
+      setIndex((prev) => prev - 1);
+      setSlides(heroSlides[index]);
+    } else {
+      setIndex(2);
+      setSlides(heroSlides[index]);
+    }
+  };
+
+  const nextSlide = () => {
+    if (index >= 0 && index < 2) {
+      setIndex((prev) => prev + 1);
+      setSlides(heroSlides[index]);
+    } else {
+      setIndex(0);
+      setSlides(heroSlides[index]);
+    }
+  };
+
   return (
     <section aria-label="Landing section of the room's website">
-      <div className='h-[50vh] bg-slate-300 bg-center bg-no-repeat bg-cover'>
+      <div className='relative h-[50vh]'>
         <Navbar />
+
+        <img
+          src={slides.img}
+          alt="the room's product catalogue"
+          className='absolute inset-0 -z-30 h-[50vh]'
+        />
       </div>
+
       <div className='container-gutters py-12'>
-        <h1 className='text-4xl font-bold'>
-          Discover innovative ways to decorate
-        </h1>
-        <p className='text-[var(--dark-gray)] text-lg mt-4'>
-          We provide unmatched quality, comfort, and style for property owners
-          accross the country. Our experts combine form and function in bringing
-          your vision to life. Create a room in your own style with our
-          collection and make your property a reflection of you and what you
-          love.
-        </p>
+        <h1 className='text-4xl font-bold'>{slides.heading}</h1>
+
+        <p className='text-[var(--dark-gray)] text-lg mt-4'>{slides.text}</p>
+
         <button className='flex items-center gap-4 mt-10'>
-          <span className='uppercase tracking-[1rem]'>shop now</span>{' '}
+          <span className='uppercase tracking-[1rem]'>shop now</span>
           <img src={arrowIcon} role='presentation' />
         </button>
       </div>
